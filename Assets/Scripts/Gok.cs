abca6e9f275ca2f5947a55d279feda4a33b140d9
Gerry Lacuarta <38984956+Gerry-ERS@users.noreply.github.com>
2025-07-15 06:28:41 -0400

    add scores display

---
+++ Assets/Scripts/Gok.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class TestScoresText : MonoBehaviour
+{
+	void Start()
+	{
+		GameObject scoresText = GameObject.FindGameObjectWithTag("ScoresText");
+		if (scoresText != null)
+			Debug.Log("Found ScoresText with tag!");
+		else
+			Debug.LogError("ScoresText not found!");
+	}
+}
---
+++ Assets/Scripts/ScoreSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Added for Button
+
+
+public class ScoreSceneManager : MonoBehaviour
+{
+    public Button backButton; // Optional: Assign to return to Menu
+
+    void Start()
+    {
+        if (backButton != null)
+            backButton.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+    }
+}
