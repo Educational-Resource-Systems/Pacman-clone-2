0a1d46d5002029ba412996606f898de4696622a9
Gerry Lacuarta <38984956+Gerry-ERS@users.noreply.github.com>
2025-07-25 21:38:42 -0400

    Update NameToTitle.cs

--- Assets/Scripts/GUI Scripts/NameToTitle.cs
+++ Assets/Scripts/GUI Scripts/NameToTitle.cs
@@ -20,12 +20,12 @@ public class NameToTitle : MonoBehaviour {
 			title.color = Color.yellow;
 			break;
 
-		case "Fizzle":
+		case "Misalingo":
 			// pink POWDER
 			title.color = new Color(200f/255f, 214f/255f, 235f/255f);
 			break;
 
-		case "Misalingo":
+		case "Fizzle":
 			//blue TEAL
 			title.color = new Color(153f/255f, 186f/255f, 169f/255f);
 			break;
@@ -35,13 +35,13 @@ public class NameToTitle : MonoBehaviour {
 			title.color = new Color(221f/255f, 182f/255f, 139f/255f);
 			break;
 		}
-		
+
 		title.text = name;
 	}
 
 	void OnMouseExit()
 	{
-		title.text = "Dose Runner";
+		title.text = "D.O.S.E. Runner";
 		title.color = Color.white;
 	}
 }
